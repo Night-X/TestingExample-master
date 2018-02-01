51337ccf790f43e694fc61b9f87f17867fcec209
xieyue5 <xieyue5@jd.com>
2018-02-01 14:39:07 +0800

    Adjust

--- app/src/main/java/com/example/testing/testingexample/MainActivity.java
+++ app/src/main/java/com/example/testing/testingexample/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.testing.testingexample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
